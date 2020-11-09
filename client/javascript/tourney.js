2efa43fa6830d7a921b4230e70144f476d206e1e
Dang592 <61894211+Dang592@users.noreply.github.com>
2020-11-09 12:51:13 -0500

    Update tourney.js

---
+++ client/javascript/tourney.js
@@ -0,0 +1,36 @@
+/*
+
+'use strict'
+
+const tournament = *thing used to get tournament*
+
+
+//TODO: Change tourney page to reflect this or change this to reflect tourney page. Depends on how Edit Tourney Button is ultimately set up.
+function renderTournament(element){
+    for(const x in tournaments){
+        const newDiv = document.createElement("div");
+        newDiv.addClass(game-list-item);
+        newDiv.innerHTML = x.name;
+        element.appendItem(newDiv);
+        //use document.addEventListener for each tournament page
+    }
+}
+
+button.addEventListener('back-to-home', event => {
+  onClick="location.href='*****Whatever the homepage url is****'" //TODO: Correct this
+});
+
+//TODO: Edit Tourney Button
+
+
+window.onload(()=>
+    renderGames(document.getElementById("fulltournamentlist"));
+);
+
+
+
+
+
+
+
+*/
