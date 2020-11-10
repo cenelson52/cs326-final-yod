883048994a82e10964ffcae5c6e53a4b55ccb21f
Dang592 <61894211+Dang592@users.noreply.github.com>
2020-11-10 02:27:24 -0500

    Update account.js

--- client/javascript/account.js
+++ client/javascript/account.js
@@ -3,9 +3,17 @@
 
 const account = however-we-get-the-account;
 
+ALTERNATIVE?
+const response = await fetch("/account");
+if (!response.ok) {
+    console.log(response.error);
+    return;
+}
+
 window.onload = function(() => 
     document.getElementById('username').innerHTML = account.username;
     document.getElementById('email').innerHTML = account.email;
     document.getElementById('DoB').innerHTML = account.DoB;
 );
 */
+
