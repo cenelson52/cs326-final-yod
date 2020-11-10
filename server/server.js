97110ea635f8ef89077b0368ae7267fdb85b0a9a
Joseph OLeary <jpoleary@umass.edu>
2020-11-10 01:16:41 -0500

    Some more tentative dummy server attempts?

--- server/server.js
+++ server/server.js
@@ -6,20 +6,23 @@ const app = express();
 
 createServer(async (req, res) => {
     const parsed = parse(req.url, true);
-    if(parsed.pathname === '/game'){
+    if(parsed.pathname === '/getgames'){
         let body = [];
         for(let x = 0; x < 5; x++){
             body.push({'name': faker.companyName, 'players': [faker.name, faker.name, faker.name]});
         }
         res.end(JSON.stringify(body));
     }
+    else if(parsed.pathname === '/game'){
+        res.end(JSON.stringify({'name': req.name, 'players': [faker.name, faker.name, faker.name]}));
+    }
     else if(parsed.pathname === '/creategame'){
-        
+        res.end(JSON.stringify(req.game));//assumes that /creategame is sending a game object, and returns it right back after the saving process (which does not exist for the dummy server)
     }
     else if(parsed.pathname === '/match'){
 
     }
-    else if(parsed.pathname === '/create match'){
+    else if(parsed.pathname === '/creatematch'){
 
     }
     else if(parsed.pathname === '/account'){
