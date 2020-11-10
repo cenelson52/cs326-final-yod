db66e41bb9f356fceb42b1c0af3e87c716ba6fc9
Joseph OLeary <jpoleary@umass.edu>
2020-11-10 00:06:51 -0500

    Example done??

--- server/server.js
+++ server/server.js
@@ -1,11 +1,31 @@
 import {createServer} from 'http';
 import {parse} from 'url';
-import {join} from 'path';
-import {writeFile, readFileSync, existsSync, fstat} from 'fs';
+import * as _express from "express";
+import * as faker from "faker";
+const app = express();
 
 createServer(async (req, res) => {
     const parsed = parse(req.url, true);
-    
-    
-    
-}).listen(8080);
+    if(parsed.pathname === '/game'){
+        let body = [];
+        for(let x = 0; x < 5; x++){
+            body.push({'name': faker.companyName, 'players': [faker.name, faker.name, faker.name]});
+        }
+        res.end(JSON.stringify(body));
+    }
+    else if(parsed.pathname === '/creategame'){
+        
+    }
+    else if(parsed.pathname === '/match'){
+
+    }
+    else if(parsed.pathname === '/create match'){
+
+    }
+    else if(parsed.pathname === '/account'){
+
+    }
+    else if(parsed.pathname === '/createaccount'){
+
+    }
+}).listen(process.env.PORT || 8080);
