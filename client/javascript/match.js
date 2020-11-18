4c2311171592e8dd3635dc427c61ca8bbd2edd00
Connor Nelson <cenelson@umass.edu>
2020-11-18 16:23:37 -0500

    Can now select players and add them to teams.

--- client/javascript/match.js
+++ client/javascript/match.js
@@ -1,11 +1,11 @@
 'use strict'
 //in place of actually fetching the data
+//const game = games[0];
+
 const game = games[0];
 const availplayers = game.players;
-const team0 = [];
-const team1 = [];
-const team2 = [];
-let selectedplayer = {'div' : null, 'team' : team0}; //0 = no team, else its team #
+const teams = [[],[],[]];
+let selectedplayer = {'player' : null,'div' : null, 'team' : null}; //0 = no team, else its team #
 
 
 function buildPlayerDiv(player){
@@ -13,6 +13,7 @@ function buildPlayerDiv(player){
     div.classList.add('player');
     div.classList.add('row-4');
     div.classList.add('unselected');
+    div.setAttribute('id', player.name);
     div.innerText = player.name;
     div.innerHTML += '  -  wins : ' + player.wins + ' | losses : ' + player.losses ;
     
@@ -20,59 +21,71 @@ function buildPlayerDiv(player){
 }
 function buildTeamPlayerDiv(player){
     const div = document.createElement('div');
-    div.classList.add('player');
+    div.classList.add('team-player');
     div.classList.add('row-4');
+    div.innerText = player.name;
     const stats = document.createElement('div');
     stats.classList.add('player-stats');
-    for(let i in game["custom-stats"]){
-        
+    //stats.innerHTML += 'wins : ' + player.wins + ' | losses : ' + player.losses + '<br>';
+    stats.innerHTML += '|';
+    for(let i in player.stats){
+        stats.innerHTML += ( i + ' : ' + player.stats[i] + '|');
     }
+    div.appendChild(stats);
+    
+
+    return div;
 }
 
-for(let i in availplayers){
-    team0.push(buildPlayerDiv(availplayers[i]));
-}
+ for(let i in availplayers){
+     teams[0].push(availplayers[i]);
+ }
 
 function renderPlayers(){
-    
+    teams[0] = fillgaps(teams[0]);
     document.getElementById('playerlist').innerHTML = '';
+    document.getElementById('team1').innerHTML = '';
+    document.getElementById('team2').innerHTML = '';
     // document.getElementById('team1').innerHTML = '';
     // document.getElementById('team2').innerHTML = '';
-    pts();
-    printTeam(team0);
-    printTeam(team1);
-    for(let i in team0){
+
+    printTeam(teams[0]);
+    printTeam(teams[1]);
+    for(let i in teams[0]){
         //console.log(team0[i]);
-        document.getElementById('playerlist').appendChild(team0[i]);
+        document.getElementById('playerlist').appendChild(buildPlayerDiv(teams[0][i]));
     }
-    // for(let i in team1){
-    //     document.getElementById('team1').appendChild(team1[i]);
-    // }
-    // for(let i in team2){
-    //     document.getElementById('team2').appendChild(team2[i]);
-    // }
+    for(let j = 1; j <=2; j++){
+        for(let i in teams[j]){
+            document.getElementById('team' + j).appendChild(buildTeamPlayerDiv(teams[j][i]));
+        }
+    }
+    addSelection();
 }
 
 renderPlayers();
 
-const playerdivs = document.getElementsByClassName('player');
-//console.log(playerdivs);
 
-for(let i = 0; i < playerdivs.length; i++){
-   // console.log(playerdivs[i])
-    playerdivs[i].addEventListener('click', () => {
+function addSelection(){
+    const playerdivs = document.getElementsByClassName('player');
+    //console.log(playerdivs);
 
-        if(selectedplayer.div === playerdivs[i]){
-            deselect();
-            
-        }else{
-            if(selectedplayer.div !== null){
+    for(let i = 0; i < playerdivs.length; i++){
+       // console.log(playerdivs[i])
+        playerdivs[i].addEventListener('click', () => {
+
+            if(selectedplayer.div === playerdivs[i]){
                 deselect();
+            
+            }else{
+                if(selectedplayer.div !== null){
+                    deselect();
+                }
+                console.log(playerdivs[i]);
+                select(playerdivs[i]);
             }
-            console.log(playerdivs[i]);
-            select(playerdivs[i]);
-        }
-    });
+        });
+    }
 }
 
 function getIndex(array, el){
@@ -84,10 +97,12 @@ function getIndex(array, el){
     return -1;
 }
 
+
+//debuging functions
 function pts(){
-    console.log('team0: ' + team0.length+ ' '+ team0);
-    console.log('team1: ' +  team1.length+ ' '+team1);
-    console.log('team2: ' +  team2.length+ ' '+team2);
+    // console.log('team0: ' + team0.length+ ' '+ team0);
+    // console.log('team1: ' +  team1.length+ ' '+team1);
+    // console.log('team2: ' +  team2.length+ ' '+team2);
 }
 function printTeam(team){
     console.log('-');
@@ -100,13 +115,13 @@ function getTeam(pdiv){
     const parent = pdiv.parentElement;
 
     if(parent === document.getElementById('playerlist')){
-        return team0;
+        return teams[0];
     }
     if(parent === document.getElementById('team1')){
-        return team1;
+        return teams[1];
     }
     if(parent === document.getElementById('team2')){
-        return team2;
+        return teams[2];
     }
     return null;
 }
@@ -123,27 +138,40 @@ function deselect(){
     selectedplayer.div = null;
     selectedplayer.team = null;
 }
+function getPfromDiv(div){
+    for(let i in availplayers){
+        if(availplayers[i].name === div.id){
+            return availplayers[i];
+        }
+    }
+    return null;
+}
+function fillgaps(arr){
+    const r = [];
+    for(let i in arr){
+        if(arr[i] !== null){
+            r.push(arr[i]);
+        }
+    }
+    return r;
+}
 
 document.getElementById('team1').addEventListener('click', () => {
-    if(selectedplayer.div !== null && selectedplayer.team !== team1){
-        selectedplayer.team.splice(getIndex(selectedplayer.div));
-        console.log('selected: ' + selectedplayer.div);//why is this an array
-        team1.push(selectedplayer.div);
-        selectedplayer.team = team1;
+    if(selectedplayer.div !== null){
+        const pl = getPfromDiv(selectedplayer.div);
+        teams[0][getIndex(teams[0], pl)] = null;
+        teams[1].push(pl);
         deselect();
-        
     }
     renderPlayers();
 });
 
 document.getElementById('team2').addEventListener('click', () => {
-    if(selectedplayer.div !== null && selectedplayer.team !== team2){
-        selectedplayer.team.splice(getIndex(selectedplayer.div));
-        console.log('selected: ' + selectedplayer.div);//why is this an array
-        team2.push(selectedplayer.div);
-        selectedplayer.team = team2;
+    if(selectedplayer.div !== null && selectedplayer.team !== teams[2]){
+        const pl = getPfromDiv(selectedplayer.div);
+        teams[0][getIndex(teams[0], pl)] = null;
+        teams[2].push(pl);
         deselect();
-        
     }
     renderPlayers();
 });
