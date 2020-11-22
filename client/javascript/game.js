f3f4de4a22f872885973f9b07ee760a52bafca5c
Connor Nelson <cenelson@umass.edu>
2020-11-22 05:51:10 -0500

    modifications to some js files

--- client/javascript/game.js
+++ client/javascript/game.js
@@ -4,134 +4,138 @@
 //retrieve the game
 //let game = games[0];//temp
 
-const games = await fetch("/games");
-if (!gamest.ok) {
-    console.log(games.error);
-    return;
-}
+window.addEventListener("load", async function () {
 
-document.getElementById('titlespot').innerText = game.name;
-
-function buildMatchDiv(match){
-    const div = document.createElement('div');
-    div.classList.add('row-4');
-    div.classList.add('match');
-    const team1 = document.createElement('div');
-    const team2 = document.createElement('div');
-    team1.classList.add('col-4');
-    team2.classList.add('col-4');
-    if(match.team1.won){
-        team1.classList.add('winner');
-        team2.classList.add('loser');
-    }else{
-        team2.classList.add('winner');
-        team1.classList.add('loser');
+    const games = await fetch("/games");
+    if (!gamest.ok) {
+        console.log(games.error);
+        return;
     }
 
-    for(let i in match.team1.players){
-        team1.appendChild(buildPlayerDiv2(match.team1.players[i]));
-    }
-    team1.innerHTML += 'score: ' + match.team1.score;
-    
-    for(let i in match.team2.players){
-        team2.appendChild(buildPlayerDiv2(match.team2.players[i]));
-    }
-    team2.innerHTML += 'score: ' + match.team2.score;
+    document.getElementById('titlespot').innerText = game.name;
 
-    
-
-
-    const space = document.createElement('div');
-    space.classList.add('col-4');
-    space.innerText = 'vs.'
-    div.appendChild(team1);
-    div.appendChild(space);
-    div.appendChild(team2);
-    div.innerHTML += 'Notes: ' + (match['match-notes']);
-    return div;
-}
-
-for(let i in game.matches){
-    document.getElementById('recordedmatches').appendChild(buildMatchDiv(game.matches[i]));
-}
-
-
-
-
-//takes in player object and returns a div
-function buildPlayerDiv(player){
-    const div = document.createElement('div');
-    div.classList.add('player');
-    div.classList.add('row-4');
-    div.innerText = player.name;
-    const stats = document.createElement('div');
-    stats.classList.add('player-stats');
-    stats.innerHTML += 'wins : ' + player.wins + ' | losses : ' + player.losses + '<br>';
-    for(let i in player.stats){
-        stats.innerHTML += ('-' + i + ' : ' + player.stats[i] + '<br>');
-    }
-    div.appendChild(stats);
-    
-
-    return div;
-}
-function buildPlayerDiv2(player){
-    const div = document.createElement('div');
-    div.classList.add('player');
-    div.classList.add('row-4');
-    div.innerText = player.name;
-    const stats = document.createElement('div');
-    stats.classList.add('player-stats');
-   
-    for(let i in player.stats){
-        stats.innerHTML += ('-' + i + ' : ' + player.stats[i] + '<br>');
-    }
-    div.appendChild(stats);
-    
-
-    return div;
-}
-
-for(let i in game.players){
-    document.getElementById('player-list').appendChild(buildPlayerDiv(game.players[i]));
-}
-
-document.getElementById('newplayer').addEventListener('click', () => {
-    const name = document.getElementById('new-player-name').value;
-    if(name === ''){
-        alert('No name entered');
-    }else{
-        const player = {'name' : name, 'wins':0, 'losses':0, 'stats':{} };
-        for(let i in game["custome-stats"]){
-            player.stats[game["custome-stats"][i]] = 0;
+    function buildMatchDiv(match){
+        const div = document.createElement('div');
+        div.classList.add('row-4');
+        div.classList.add('match');
+        const team1 = document.createElement('div');
+        const team2 = document.createElement('div');
+        team1.classList.add('col-4');
+        team2.classList.add('col-4');
+        if(match.team1.won){
+            team1.classList.add('winner');
+            team2.classList.add('loser');
+        }else{
+            team2.classList.add('winner');
+            team1.classList.add('loser');
         }
-        //server stuff here
-        game.players.push(player);//temp
-    }
-    
 
-    //for testing purposes
-    document.getElementById('player-list').innerHTML = '';
+        for(let i in match.team1.players){
+            team1.appendChild(buildPlayerDiv2(match.team1.players[i]));
+        }
+        team1.innerHTML += 'score: ' + match.team1.score;
+        
+        for(let i in match.team2.players){
+            team2.appendChild(buildPlayerDiv2(match.team2.players[i]));
+        }
+        team2.innerHTML += 'score: ' + match.team2.score;
+
+        
+
+
+        const space = document.createElement('div');
+        space.classList.add('col-4');
+        space.innerText = 'vs.'
+        div.appendChild(team1);
+        div.appendChild(space);
+        div.appendChild(team2);
+        div.innerHTML += 'Notes: ' + (match['match-notes']);
+        return div;
+    }
+
+    for(let i in game.matches){
+        document.getElementById('recordedmatches').appendChild(buildMatchDiv(game.matches[i]));
+    }
+
+
+
+
+    //takes in player object and returns a div
+    function buildPlayerDiv(player){
+        const div = document.createElement('div');
+        div.classList.add('player');
+        div.classList.add('row-4');
+        div.innerText = player.name;
+        const stats = document.createElement('div');
+        stats.classList.add('player-stats');
+        stats.innerHTML += 'wins : ' + player.wins + ' | losses : ' + player.losses + '<br>';
+        for(let i in player.stats){
+            stats.innerHTML += ('-' + i + ' : ' + player.stats[i] + '<br>');
+        }
+        div.appendChild(stats);
+        
+
+        return div;
+    }
+    function buildPlayerDiv2(player){
+        const div = document.createElement('div');
+        div.classList.add('player');
+        div.classList.add('row-4');
+        div.innerText = player.name;
+        const stats = document.createElement('div');
+        stats.classList.add('player-stats');
+    
+        for(let i in player.stats){
+            stats.innerHTML += ('-' + i + ' : ' + player.stats[i] + '<br>');
+        }
+        div.appendChild(stats);
+        
+
+        return div;
+    }
+
     for(let i in game.players){
         document.getElementById('player-list').appendChild(buildPlayerDiv(game.players[i]));
     }
-});
 
-
-
-document.getElementById('player-search').addEventListener('keyup', () => {
-    document.getElementById('player-list').innerHTML = '';
-    const search = document.getElementById('player-search').value;
-    //const results = [];
-    for(let i in game.players){
-        if(game.players[i].name.includes(search)){
-            document.getElementById('player-list').appendChild(buildPlayerDiv(game.players[i]));
-            //results.push(game.players[i]);
+    document.getElementById('newplayer').addEventListener('click', () => {
+        const name = document.getElementById('new-player-name').value;
+        if(name === ''){
+            alert('No name entered');
+        }else{
+            const player = {'name' : name, 'wins':0, 'losses':0, 'stats':{} };
+            for(let i in game["custome-stats"]){
+                player.stats[game["custome-stats"][i]] = 0;
+            }
+            //server stuff here
+            game.players.push(player);//temp
         }
-    }
+        
 
-});
+        //for testing purposes
+        document.getElementById('player-list').innerHTML = '';
+        for(let i in game.players){
+            document.getElementById('player-list').appendChild(buildPlayerDiv(game.players[i]));
+        }
+    });
 
-document.getElementById('recordmatch').addEventListener('click', () => {
-    //will route to match.html
+
+
+    document.getElementById('player-search').addEventListener('keyup', () => {
+        document.getElementById('player-list').innerHTML = '';
+        const search = document.getElementById('player-search').value;
+        //const results = [];
+        for(let i in game.players){
+            if(game.players[i].name.includes(search)){
+                document.getElementById('player-list').appendChild(buildPlayerDiv(game.players[i]));
+                //results.push(game.players[i]);
+            }
+        }
+
+    });
+
+    document.getElementById('recordmatch').addEventListener('click', () => {
+        //will route to match.html
+        window.location.href = '/match.html'
+    });
 });
--- client/javascript/gamecreate.js
+++ client/javascript/gamecreate.js
@@ -2,38 +2,47 @@
 
 
 
-const stats = []; //strings with stat names
+window.addEventListener("load", async function () {
 
-
-document.getElementById('addstatbtn').addEventListener('click', () => {
-    const statname = document.getElementById('stat-input').value;
-    if(statname === ''){
-        alert('no stat name entered');
-    }else{
-        const statdiv = document.createElement('div');
-        statdiv.innerText = statname;
-        statdiv.classList.add('custom-stats');
-        document.getElementById('stats').appendChild(statdiv);
-        document.getElementById('stat-input').value = '';
-        stats.push(statname);
+    const response = await fetch('/gamecreate.html');
+    if (!response.ok) {
+        console.log(response.error);
+        return;
     }
-});
 
-document.getElementById('newgamebtn').addEventListener('click', () => {
-    const game = {}
-    const name = document.getElementById('name').value;
-    const desc = document.getElementById('description').value
-    if(name === ''){
-        alert('no name entered');
-    }else{
-        game['name'] = name;
-        game['description'] = desc;
-        game['custom-stats'] = stats;
-        game['players'] = [];
-        console.log(game);
+    const stats = []; //strings with stat names
 
-        //this is where the game will be saved into db
-        window.location.href = "/game.html";
-    }
-    
+
+    document.getElementById('addstatbtn').addEventListener('click', () => {
+        const statname = document.getElementById('stat-input').value;
+        if(statname === ''){
+            alert('no stat name entered');
+        }else{
+            const statdiv = document.createElement('div');
+            statdiv.innerText = statname;
+            statdiv.classList.add('custom-stats');
+            document.getElementById('stats').appendChild(statdiv);
+            document.getElementById('stat-input').value = '';
+            stats.push(statname);
+        }
+    });
+
+    document.getElementById('newgamebtn').addEventListener('click', () => {
+        const game = {}
+        const name = document.getElementById('name').value;
+        const desc = document.getElementById('description').value
+        if(name === ''){
+            alert('no name entered');
+        }else{
+            game['name'] = name;
+            game['description'] = desc;
+            game['custom-stats'] = stats;
+            game['players'] = [];
+            console.log(game);
+
+            //this is where the game will be saved into db
+            window.location.href = "/game.html";
+        }
+        
+    });
 });
